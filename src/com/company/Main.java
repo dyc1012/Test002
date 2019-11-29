3bd0955d49ce822655fddafebe49dd33e331427f
dyc1012 <dyc1012@163.com>
2019-11-29 19:03:25 +0800

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(new Date() + "-->helloxxxxx~");
+        System.out.println(new Date() + "-->helloxsdfxxxx~");
 
     }
 }
